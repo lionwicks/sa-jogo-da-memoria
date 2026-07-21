aba69649b758ff4e98b097f55e7bca118b587b02
Aguiar019 <roger.a.nascimento@alumo.senai.br>
2026-07-21 15:50:40 -0300

    Adicionado using System

--- CodJogoDaMemoria/obj/Debug/net10.0/CodJogoDaMemoria.AssemblyInfo.cs
+++ CodJogoDaMemoria/obj/Debug/net10.0/CodJogoDaMemoria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodJogoDaMemoria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2d7e3737ac17bbbf0bd2c175b86c05a7558eea3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodJogoDaMemoria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodJogoDaMemoria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
